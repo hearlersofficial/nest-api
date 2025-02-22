bd48a60436eff90bf84276bed5f9567b1327e1ee
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-02-22 18:11:52 +0900

    feat: context findMany 메소드 추가
    - name 으로 조회가능하도록

--- src/services/counselings/aggregates/contexts/contexts.module.ts
+++ src/services/counselings/aggregates/contexts/contexts.module.ts
@@ -1,4 +1,7 @@
 import { ContextEntity } from "~shared/core/infrastructure/entities/prompts/Contexts.entity";
+import { ContextService } from "~counselings/aggregates/contexts/applications/context.service";
+import { ContextPersistor } from "~counselings/aggregates/contexts/applications/tools/context.persistor";
+import { ContextReader } from "~counselings/aggregates/contexts/applications/tools/context.reader";
 import { PsqlContextRepositoryAdaptor } from "~counselings/aggregates/contexts/infrastructures/adaptors/psql.context.repository.adaptor";
 import { CONTEXT_REPOSITORY } from "~counselings/aggregates/contexts/infrastructures/context.repository.port";
 
@@ -8,11 +11,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 @Module({
   imports: [TypeOrmModule.forFeature([ContextEntity])],
   providers: [
+    ContextPersistor,
+    ContextReader,
+    ContextService,
     {
       provide: CONTEXT_REPOSITORY,
       useClass: PsqlContextRepositoryAdaptor,
     },
   ],
-  exports: [],
+  exports: [ContextService],
 })
 export class ContextsModule {}

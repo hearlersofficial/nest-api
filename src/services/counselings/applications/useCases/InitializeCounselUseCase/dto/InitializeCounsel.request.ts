3ec823349fe02eddac2166e05f33ebfcd2a632f5
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-03-04 22:06:41 +0900

    refactor: 사용안하는 useCase 삭제 및 모듈 import 설정

--- src/services/counselings/applications/useCases/InitializeCounselUseCase/dto/InitializeCounsel.request.ts
+++
@@ -1,6 +0,0 @@
-import { UniqueEntityId } from "~shared/core/domain/UniqueEntityId";
-import { Counselors } from "~counselings/aggregates/counselors/domain/counselors";
-export interface InitializeCounselUseCaseRequest {
-  userId: UniqueEntityId;
-  counselor: Counselors;
-}
--- src/services/counselings/applications/useCases/InitializeCounselWithBubbleUseCase/dto/InitializeCounselWithBubble.response.ts
+++
@@ -1,8 +0,0 @@
-import { UseCaseCoreResponse } from "~shared/core/applications/UseCase.response";
-import { CounselMessages } from "~counselings/aggregates/counselMessages/domain/CounselMessages";
-import { Counsels } from "~counselings/aggregates/counsels/domain/Counsels";
-
-export interface InitializeCounselWithBubbleUseCaseResponse extends UseCaseCoreResponse {
-  counsel?: Counsels;
-  counselMessages?: CounselMessages[];
-}

4ed3dff5beb94587094b6f0be10a4ceb21b9665f
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-03-04 22:05:33 +0900

    refactor: 상담 메시지 생성 command 구조 변경

--- src/services/counselings/applications/commands/CreateMessage/CreateMessage.handler.ts
+++ src/services/counselings/applications/commands/CreateMessage/CreateMessage.handler.ts
@@ -1,126 +1,36 @@
 import { HttpStatusBasedRpcException } from "~shared/filters/exceptions";
-import { CreateCounselMessageUseCase } from "~counselings/aggregates/counselMessages/applications/useCases/CreateCounselMessageUseCase/CreateCounselMessageUseCase";
-import { GetCounselMessageListUseCase } from "~counselings/aggregates/counselMessages/applications/useCases/GetCounselMessageListUseCase/GetCounselMessageListUseCase";
-import { CounselMessages } from "~counselings/aggregates/counselMessages/domain/CounselMessages";
-import { GetCounselorUseCase } from "~counselings/aggregates/counselors/applications/useCases/GetCounselorUseCase/GetCounselorUseCase";
-import { GetCounselUseCase } from "~counselings/aggregates/counsels/applications/useCases/GetCounselUseCase/GetCounselUseCase";
-import { UpdateCounselUseCase } from "~counselings/aggregates/counsels/applications/useCases/UpdateCounselUseCase/UpdateCounselUseCase";
-import { CreateMessageCommand } from "~counselings/applications/commands/CreateMessage/CreateMessage.command";
-import { GenerateGptResponseUseCase } from "~counselings/applications/useCases/GenerateGptResponseUseCase/GenerateGptResponseUseCase";
+import { CounselService } from "~counselings/aggregates/counsels/applications/counsel.service";
+import { CreateMessageCommand, CreateMessageCommandResult } from "~counselings/applications/commands/CreateMessage/CreateMessage.command";
+import { ProceedCounselingUseCase } from "~counselings/applications/useCases/ProceedCounselingUseCase/ProceedCounselingUseCase";
 
 import { HttpStatus } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { ChatCompletionMessageParam } from "openai/resources";
 
 @CommandHandler(CreateMessageCommand)
 export class CreateMessageHandler implements ICommandHandler<CreateMessageCommand> {
-  constructor(
-    private readonly getCounselUseCase: GetCounselUseCase,
-    private readonly getCounselorUseCase: GetCounselorUseCase,
-    private readonly createCounselMessageUseCase: CreateCounselMessageUseCase,
-    private readonly getCounselMessageListUseCase: GetCounselMessageListUseCase,
-    private readonly updateCounselUseCase: UpdateCounselUseCase,
-    private readonly generateGptResponseUseCase: GenerateGptResponseUseCase,
-  ) {}
+  constructor(private readonly counselService: CounselService, private readonly proceedCounselingUseCase: ProceedCounselingUseCase) {}
 
-  async execute(command: CreateMessageCommand): Promise<CounselMessages> {
+  async execute(command: CreateMessageCommand): Promise<CreateMessageCommandResult> {
     const { counselId, message } = command.props;
 
     // 상담 정보 가져오기
-    const getCounselResult = await this.getCounselUseCase.execute({ counselId });
-    if (!getCounselResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, getCounselResult.error);
+    const counsel = await this.counselService.findOne(counselId);
+    if (!counsel) {
+      throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "Counsel not found");
     }
-    const counsel = getCounselResult.counsel;
 
-    // 사용자 메시지 추가
-    const createUserMessageResult = await this.createCounselMessageUseCase.execute({
-      counselId,
-      userId: counsel.userId,
-      message,
-      isUserMessage: true,
+    // 상담 진행
+    const proceedCounselingResponse = await this.proceedCounselingUseCase.execute({
+      counsel,
+      userMessage: message,
     });
-    if (!createUserMessageResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, createUserMessageResult.error);
-    }
-    const userMessage = createUserMessageResult.counselMessage;
-
-    // 상담 단계 초기화여부 체크
-    if (counsel.checkNeedStageReset()) {
-      // counsel.updateCounselStage(CounselStage.SMALL_TALK);
+    if (!proceedCounselingResponse.ok) {
+      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, proceedCounselingResponse.error);
     }
 
-    // 극단적 상태 체크
-    if (userMessage.checkExtreme()) {
-      // counsel.updateCounselStage(CounselStage.EXTREME);
-    }
-
-    // const stage = counsel.counselStage;
-
-    // 상담사 정보 가져오기
-    const getCounselorResult = await this.getCounselorUseCase.execute({ counselorId: counsel.counselorId });
-    if (!getCounselorResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, getCounselorResult.error);
-    }
-    // const counselor = getCounselorResult.counselor;
-
-    // 시스템 프롬프트 가져오기
-    // 유저 정보 가져와 집어넣는 로직 필요(프롬프트에서 사용하는 유저 정보 구체화 필요)
-    const prompts: ChatCompletionMessageParam[] = [];
-    // const systemPromptType = counselor.decideSystemPrompt(stage);
-    // const getPromptResult = await this.getCounselPromptByTypeUseCase.execute({ promptType: systemPromptType });
-    // if (!getPromptResult.ok) {
-    //   throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, getPromptResult.error);
-    // }
-    // const systemPrompt = getPromptResult.counselPrompt;
-    // prompts.push(systemPrompt.makePrompt(counselor));
-
-    // 이전 대화 가져오기 (유저의 대화만 가져오는지 시스템 응답도 포함하는지 확인 필요)
-    const getMessageListResult = await this.getCounselMessageListUseCase.execute({ counselId });
-    if (!getMessageListResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, getMessageListResult.error);
-    }
-    const messageList = getMessageListResult.counselMessageList;
-    messageList.forEach((message) => {
-      prompts.push(message.makePrompt());
-    });
-
-    // 응답 생성
-    const generateGptResponseResult = await this.generateGptResponseUseCase.execute({ prompts });
-    if (!generateGptResponseResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, generateGptResponseResult.error);
-    }
-    const response = generateGptResponseResult.response;
-
-    // 시스템 메시지 추가
-    const createSystemMessageResult = await this.createCounselMessageUseCase.execute({
-      counselId,
-      userId: counsel.userId,
-      message: response,
-      isUserMessage: false,
-    });
-    if (!createSystemMessageResult.ok) {
-      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, createSystemMessageResult.error);
-    }
-    const systemMessage = createSystemMessageResult.counselMessage;
-
-    // 프롬프트 분기(SMALL_TALK  단계에서만 이루어지는지 확인 필요)
-    // if (stage == CounselStage.SMALL_TALK && systemMessage.checkNeedBranch()) {
-    // const branchCounselStageResult = await this.branchCounselStageUseCase.execute({ prompts: prompts.slice(1) });
-    // if (!branchCounselStageResult.ok) {
-    //   throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, branchCounselStageResult.error);
-    // }
-    // const branchedStage = branchCounselStageResult.branchedStage;
-    // counsel.updateCounselStage(branchedStage);
-    // }
-
-    // last message 저장 및 상담 정보 업데이트
-    // counsel.saveLastMessage(systemMessage);
-    // const updateCounselResult = await this.updateCounselUseCase.execute({ toUpdateCounsel: counsel });
-    // if (!updateCounselResult.ok) {
-    //   throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, updateCounselResult.error);
-    // }
-
-    return systemMessage;
+    return {
+      createdCounselMessage: proceedCounselingResponse.createdCounselMessage,
+      counselorResponseMessage: proceedCounselingResponse.counselorResponseMessage,
+    };
   }
 }

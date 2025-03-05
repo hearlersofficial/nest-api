9faf692394385b6a7bb344d734db1582564ebe25
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-03-05 16:37:48 +0900

    fix: instruction domain 에 name property 추가

--- src/services/counselings/presentations/grpc/schema.counsels.mapper.ts
+++ src/services/counselings/presentations/grpc/schema.counsels.mapper.ts
@@ -1,15 +1,21 @@
+import { Contexts } from "~counselings/aggregates/contexts/domain/contexts";
 import { CounselMessages } from "~counselings/aggregates/counselMessages/domain/CounselMessages";
 import { Counselors } from "~counselings/aggregates/counselors/domain/counselors";
 import { Counsels } from "~counselings/aggregates/counsels/domain/Counsels";
+import { CounselTechniques } from "~counselings/aggregates/counselTechniques/domain/counselTechniques";
 import { Personas } from "~counselings/aggregates/personas/domain/personas";
 import { Tones } from "~counselings/aggregates/tones/domain/tones";
 import {
+  Context,
+  ContextSchema,
   Counsel,
   CounselMessage,
   CounselMessageSchema,
   Counselor,
   CounselorSchema,
   CounselSchema,
+  CounselTechnique,
+  CounselTechniqueSchema,
   Persona,
   PersonaSchema,
   Tone,
@@ -26,6 +32,8 @@ export class SchemaCounselsMapper {
       counselorId: counsel.counselorId.getString(),
       lastMessage: counsel.lastMessage,
       lastChatedAt: counsel.lastChatedAt ? counsel.lastChatedAt.toISOString() : null,
+      counselTechniqueId: counsel.counselTechniqueId.getString(),
+      counselorUserRelationshipId: counsel.counselorUserRelationshipId.getString(),
       createdAt: counsel.createdAt.toISOString(),
       updatedAt: counsel.updatedAt.toISOString(),
       deletedAt: counsel.deletedAt ? counsel.deletedAt.toISOString() : null,
@@ -50,6 +58,7 @@ export class SchemaCounselsMapper {
     const bubble = counselor.bubble;
     return create(CounselorSchema, {
       id: counselor.id.getString(),
+      toneId: counselor.toneId.getString(),
       name: counselor.name,
       description: counselor.description,
       gender: counselor.gender,
@@ -59,6 +68,46 @@ export class SchemaCounselsMapper {
       createdAt: counselor.createdAt.toISOString(),
       updatedAt: counselor.updatedAt.toISOString(),
       deletedAt: counselor.deletedAt ? counselor.deletedAt.toISOString() : null,
+    });
+  }
+
+  static toCounselTechniqueProto(counselTechnique: CounselTechniques): CounselTechnique {
+    return create(CounselTechniqueSchema, {
+      id: counselTechnique.id.getString(),
+      name: counselTechnique.name,
+      toneId: counselTechnique.toneId ? counselTechnique.toneId.getString() : null,
+      contextId: counselTechnique.contextId.getString(),
+      instructionId: counselTechnique.instructionId.getString(),
+      counselTechniqueStage: counselTechnique.counselTechniqueStage,
+      nextCounselTechniqueId: counselTechnique.nextTechniqueId ? counselTechnique.nextTechniqueId.getString() : null,
+      createdAt: counselTechnique.createdAt.toISOString(),
+      updatedAt: counselTechnique.updatedAt.toISOString(),
+      deletedAt: counselTechnique.deletedAt ? counselTechnique.deletedAt.toISOString() : null,
+    });
+  }
+
+  // TODO: counselor user relationship mapper 추가
+
+  static toPersonaProto(persona: Personas): Persona {
+    return create(PersonaSchema, {
+      id: persona.id.getString(),
+      body: persona.body,
+      counselorId: persona.counselorId.getString(),
+      createdAt: persona.createdAt.toISOString(),
+      updatedAt: persona.updatedAt.toISOString(),
+      deletedAt: persona.deletedAt ? persona.deletedAt.toISOString() : null,
+    });
+  }
+
+  static toContextProto(context: Contexts): Context {
+    return create(ContextSchema, {
+      id: context.id.getString(),
+      name: context.name,
+      placeholders: context.placeholders,
+      body: context.body,
+      createdAt: context.createdAt.toISOString(),
+      updatedAt: context.updatedAt.toISOString(),
+      deletedAt: context.deletedAt ? context.deletedAt.toISOString() : null,
     });
   }
 

e6a4a925ae8cd494a67398098da06ed5a216dea9
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-03-15 15:57:16 +0900

    fix: counselor user relationship 비활성화
    - TODO: 유저 생성 이벤트 리스너 추가 이후 활성화

--- src/shared/core/infrastructure/entities/CounselorUserRelationships.entity.ts
+++ src/shared/core/infrastructure/entities/CounselorUserRelationships.entity.ts
@@ -26,6 +26,6 @@ export class CounselorUserRelationshipsEntity extends CoreEntity {
   @Column({ type: "int", name: "rapport", comment: "상담사와의 관계 정도" })
   rapport: number;
 
-  @OneToMany(() => CounselsEntity, (counsel) => counsel.counselorUserRelationship)
-  counsels: CounselsEntity[];
+  // @OneToMany(() => CounselsEntity, (counsel) => counsel.counselorUserRelationship)
+  // counsels: CounselsEntity[];
 }

1a79b25b19742dd2df4ac1fb8d1a007b31a25454
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-03-28 21:14:58 +0900

    remove: 안쓰는 파일 삭제

--- src/services/counselings/applications/types/instruction.type.ts
+++
@@ -1,7 +0,0 @@
-import { InstructionItems } from "~counselings/domains/instructionItems/models/instructionItems";
-import { Instructions } from "~counselings/domains/instructions/models/instructions";
-
-export type InstructionWithItems = {
-  instruction: Instructions;
-  instructionItems: InstructionItems[];
-};
--- src/shared/enums/CounselorGender.enum.ts
+++
@@ -1,8 +0,0 @@
-import { CounselorGender } from "~proto/com/hearlers/v1/model/counsel_pb";
-
-export const CounselorGenderMap = {
-  [CounselorGender.UNSPECIFIED]: "unspecified",
-  [CounselorGender.MALE]: "male",
-  [CounselorGender.FEMALE]: "female",
-  [CounselorGender.NONE]: "none",
-};

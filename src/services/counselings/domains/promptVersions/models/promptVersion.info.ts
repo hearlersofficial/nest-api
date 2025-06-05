c0eadfe2e9bba4530e2b03c82c8a2cd69a71ac90
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-06-05 15:12:35 +0900

    refactor: promptVersion 도메인 서비스 로직 캡슐화

---
+++ src/services/counselings/domains/promptVersions/models/promptVersion.info.ts
@@ -0,0 +1,44 @@
+import { CounselorScopedPromptInfo } from "~counselings/domains/promptVersions/models/counselorScopedPrompt.info";
+import { PromptVersions } from "~counselings/domains/promptVersions/models/promptVersions";
+import { ToneScopedPromptInfo } from "~counselings/domains/promptVersions/models/toneScopedPrompt.info";
+import { GPTModel } from "~proto/com/hearlers/v1/model/counsel_prompt_pb";
+
+import { Dayjs } from "dayjs";
+
+export class PromptVersionInfo {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly description: string,
+    public readonly counselorScopedPrompts: CounselorScopedPromptInfo[],
+    public readonly toneScopedPrompts: ToneScopedPromptInfo[],
+    public readonly isActive: boolean,
+    public readonly isTemporary: boolean,
+    public readonly isBookmarked: boolean,
+    public readonly gptModel: GPTModel,
+    public readonly createdAt: Dayjs,
+    public readonly updatedAt: Dayjs,
+    public readonly deletedAt: Dayjs | null,
+  ) {}
+
+  static fromDomain(promptVersion: PromptVersions): PromptVersionInfo {
+    return new PromptVersionInfo(
+      promptVersion.id.getString(),
+      promptVersion.name,
+      promptVersion.description,
+      CounselorScopedPromptInfo.fromDomainArray(promptVersion.counselorScopedPrompts),
+      ToneScopedPromptInfo.fromDomainArray(promptVersion.toneScopedPrompts),
+      promptVersion.isActive,
+      promptVersion.isTemporary,
+      promptVersion.isBookmarked,
+      promptVersion.gptModel,
+      promptVersion.createdAt,
+      promptVersion.updatedAt,
+      promptVersion.deletedAt,
+    );
+  }
+
+  static fromDomainArray(promptVersions: PromptVersions[]): PromptVersionInfo[] {
+    return promptVersions.map((version) => PromptVersionInfo.fromDomain(version));
+  }
+}
--- src/services/counselings/domains/promptVersions/promptVersions.service.ts
+++ src/services/counselings/domains/promptVersions/promptVersions.service.ts
@@ -1,11 +1,14 @@
+import { PromptVersionInfo } from "~counselings/domains/promptVersions/models/promptVersion.info";
 import { PromptVersions, PromptVersionsNewProps } from "~counselings/domains/promptVersions/models/promptVersions";
 import { PromptVersionsCriteriaFindMany } from "~counselings/domains/promptVersions/promptVersions.criteria";
 import { PromptVersionsPersister } from "~counselings/domains/promptVersions/promptVersions.persister";
 import { PromptVersionsReader } from "~counselings/domains/promptVersions/promptVersions.reader";
+import { GPTModel } from "~proto/com/hearlers/v1/model/counsel_prompt_pb";
 
 import { HttpStatus, Injectable } from "@nestjs/common";
 import { UniqueEntityId } from "~common/shared-kernel/domains/unique-entity-id";
 import { HttpStatusBasedRpcException } from "~common/system/filters/exceptions";
+import { Transactional } from "typeorm-transactional";
 
 @Injectable()
 export class PromptVersionsService {
@@ -14,82 +17,171 @@ export class PromptVersionsService {
     private readonly promptVersionsPersister: PromptVersionsPersister,
   ) {}
 
-  async create(newProps: PromptVersionsNewProps): Promise<PromptVersions> {
-    return this.promptVersionsPersister.create(newProps);
-  }
-
-  async update(promptVersion: PromptVersions): Promise<PromptVersions> {
-    return this.promptVersionsPersister.update(promptVersion);
-  }
-
-  async updateMany(promptVersions: PromptVersions[]): Promise<PromptVersions[]> {
-    return this.promptVersionsPersister.updateMany(promptVersions);
-  }
-
-  async findOne(props: { promptVersionId: UniqueEntityId }): Promise<PromptVersions | null> {
-    return this.promptVersionsReader.findOne(props);
-  }
-
-  async getOne(props: { promptVersionId: UniqueEntityId }): Promise<PromptVersions> {
-    const promptVersion = await this.findOne(props);
+  async getOne(props: { promptVersionId: UniqueEntityId }): Promise<PromptVersionInfo> {
+    const promptVersion = await this.promptVersionsReader.findOne(props);
     if (!promptVersion) {
       throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
     }
-    return promptVersion;
+    return PromptVersionInfo.fromDomain(promptVersion);
   }
 
-  async findMany(props: PromptVersionsCriteriaFindMany): Promise<PromptVersions[]> {
-    return this.promptVersionsReader.findMany(props);
+  async getMany(props: PromptVersionsCriteriaFindMany): Promise<PromptVersionInfo[]> {
+    const promptVersions = await this.promptVersionsReader.findMany(props);
+    return PromptVersionInfo.fromDomainArray(promptVersions);
   }
 
-  async getMany(props: PromptVersionsCriteriaFindMany): Promise<PromptVersions[]> {
-    const promptVersions = await this.findMany(props);
-    // ID 값들로만 조회할 때 존재여부 검증
-    if (
-      props.ids !== undefined &&
-      props.name === undefined &&
-      props.isActive === undefined &&
-      props.isTemporary === undefined &&
-      props.isBookmarked === undefined
-    ) {
-      if (promptVersions.length !== new Set(props.ids).size) {
-        throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
-      }
-    }
-    return promptVersions;
-  }
-
-  async findActiveOne(): Promise<PromptVersions | null> {
-    const existingActiveVersions = await this.findMany({ isActive: true });
+  private async findActiveOne(): Promise<PromptVersions | null> {
+    const existingActiveVersions = await this.promptVersionsReader.findMany({ isActive: true });
     if (existingActiveVersions.length > 0) {
       return existingActiveVersions[0];
     }
     return null;
   }
 
-  async getActiveOne(): Promise<PromptVersions> {
+  async getActiveOne(): Promise<PromptVersionInfo> {
     const activeVersion = await this.findActiveOne();
     if (!activeVersion) {
       throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "Active PromptVersion not found");
     }
-    return activeVersion;
+    return PromptVersionInfo.fromDomain(activeVersion);
   }
 
-  async getTemporaryOne(): Promise<PromptVersions> {
-    const existingTemporaryVersions = await this.findMany({ isTemporary: true });
+  @Transactional()
+  async findTemporaryOne(): Promise<PromptVersions> {
+    const existingTemporaryVersions = await this.promptVersionsReader.findMany({ isTemporary: true });
     if (existingTemporaryVersions.length > 0) {
       return existingTemporaryVersions[0];
     }
 
     // 수정중인 임시버전이 없을 경우 새롭게 생성
-    const newTemporaryVersion = await this.create({});
+    const newTemporaryVersion = await this.promptVersionsPersister.create({});
 
     // 활성화 버전이 있을 경우 해당 내용 복사
     const activeVersion = await this.findActiveOne();
     if (activeVersion) {
       newTemporaryVersion.clonePrompts(activeVersion);
-      await this.update(newTemporaryVersion);
+      await this.promptVersionsPersister.update(newTemporaryVersion);
     }
     return newTemporaryVersion;
+  }
+
+  async getTemporaryOne(): Promise<PromptVersionInfo> {
+    const temporaryVersion = await this.findTemporaryOne();
+    return PromptVersionInfo.fromDomain(temporaryVersion);
+  }
+
+  @Transactional()
+  async loadExistingPromptVersion(props: { promptVersionId: UniqueEntityId }): Promise<PromptVersionInfo> {
+    const { promptVersionId } = props;
+    const promptVersion = await this.promptVersionsReader.findOne({ promptVersionId });
+    if (!promptVersion) {
+      throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
+    }
+
+    if (promptVersion.isTemporary) {
+      throw new HttpStatusBasedRpcException(HttpStatus.BAD_REQUEST, "Cannot load a temporary version");
+    }
+
+    const temporaryVersion = await this.findTemporaryOne();
+    temporaryVersion.clonePrompts(promptVersion);
+    await this.promptVersionsPersister.update(temporaryVersion);
+
+    return PromptVersionInfo.fromDomain(temporaryVersion);
+  }
+
+  @Transactional()
+  async saveTemporaryPromptVersion(props: {
+    name: string;
+    description: string;
+    isBookmarked: boolean;
+    gptModel: GPTModel;
+  }): Promise<PromptVersionInfo> {
+    const { name, description, isBookmarked, gptModel } = props;
+
+    const temporaryVersion = await this.findTemporaryOne();
+    const saveVersionResult = temporaryVersion.saveVersion({
+      name,
+      description,
+      isBookmarked,
+      gptModel,
+    });
+    if (saveVersionResult.isFailure) {
+      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, saveVersionResult.error as string);
+    }
+    await this.promptVersionsPersister.update(temporaryVersion);
+
+    return PromptVersionInfo.fromDomain(temporaryVersion);
+  }
+
+  @Transactional()
+  async updatePromptVersion(props: {
+    promptVersionId: UniqueEntityId;
+    name?: string;
+    description?: string;
+    isBookmarked?: boolean;
+    gptModel?: GPTModel;
+  }): Promise<PromptVersionInfo> {
+    const { promptVersionId, name, description, isBookmarked, gptModel } = props;
+    const promptVersion = await this.promptVersionsReader.findOne({ promptVersionId });
+    if (!promptVersion) {
+      throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
+    }
+
+    const updateResult = promptVersion.updateBasicInfo({
+      name,
+      description,
+      isBookmarked,
+      gptModel,
+    });
+    if (updateResult.isFailure) {
+      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, updateResult.error as string);
+    }
+
+    await this.promptVersionsPersister.update(promptVersion);
+    return PromptVersionInfo.fromDomain(promptVersion);
+  }
+
+  @Transactional()
+  async activatePromptVersion(props: { promptVersionId: UniqueEntityId }): Promise<PromptVersionInfo> {
+    const { promptVersionId } = props;
+    const promptVersion = await this.promptVersionsReader.findOne({ promptVersionId });
+    if (!promptVersion) {
+      throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
+    }
+
+    const activeVersions = await this.promptVersionsReader.findMany({ isActive: true });
+    if (activeVersions.length > 0) {
+      for (const activeVersion of activeVersions) {
+        const deactivateResult = activeVersion.deactivate();
+        if (deactivateResult.isFailure) {
+          throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, deactivateResult.error as string);
+        }
+        await this.promptVersionsPersister.update(activeVersion);
+      }
+    }
+    const activateResult = promptVersion.activate();
+    if (activateResult.isFailure) {
+      throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, activateResult.error as string);
+    }
+    await this.promptVersionsPersister.update(promptVersion);
+
+    return PromptVersionInfo.fromDomain(promptVersion);
+  }
+
+  @Transactional()
+  async deletePromptVersions(props: { promptVersionIds: UniqueEntityId[] }): Promise<void> {
+    const { promptVersionIds } = props;
+    const promptVersions = await this.promptVersionsReader.findMany({ ids: promptVersionIds });
+    if (promptVersions.length !== new Set(promptVersionIds.map((id) => id.getString())).size) {
+      throw new HttpStatusBasedRpcException(HttpStatus.NOT_FOUND, "PromptVersion not found");
+    }
+
+    for (const promptVersion of promptVersions) {
+      const deleteResult = promptVersion.delete();
+      if (deleteResult.isFailure) {
+        throw new HttpStatusBasedRpcException(HttpStatus.INTERNAL_SERVER_ERROR, deleteResult.error as string);
+      }
+    }
+    await this.promptVersionsPersister.updateMany(promptVersions);
   }
 }

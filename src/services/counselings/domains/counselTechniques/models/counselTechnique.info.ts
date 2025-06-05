b7b0bba9a5bc012ee7302b18be44cc3c0e22bf65
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-06-05 16:33:58 +0900

    refactor: counselTechnique 도메인 서비스 로직 캡슐화

---
+++ src/services/counselings/domains/counselTechniques/models/counselTechnique.info.ts
@@ -0,0 +1,39 @@
+import { CounselTechniques } from "~counselings/domains/counselTechniques/models/counselTechniques";
+
+import { Dayjs } from "dayjs";
+
+export class CounselTechniqueInfo {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly toneId: string,
+    public readonly context: string,
+    public readonly instruction: string,
+    public readonly messageThreshold: number,
+    public readonly nextTechniqueId: string | null,
+    public readonly isTemporary: boolean,
+    public readonly createdAt: Dayjs,
+    public readonly updatedAt: Dayjs,
+    public readonly deletedAt: Dayjs | null,
+  ) {}
+
+  static fromDomain(counselTechnique: CounselTechniques): CounselTechniqueInfo {
+    return new CounselTechniqueInfo(
+      counselTechnique.id.getString(),
+      counselTechnique.name,
+      counselTechnique.toneId.getString(),
+      counselTechnique.context,
+      counselTechnique.instruction,
+      counselTechnique.messageThreshold,
+      counselTechnique.nextTechniqueId ? counselTechnique.nextTechniqueId.getString() : null,
+      counselTechnique.isTemporary,
+      counselTechnique.createdAt,
+      counselTechnique.updatedAt,
+      counselTechnique.deletedAt,
+    );
+  }
+
+  static fromDomainArray(counselTechniques: CounselTechniques[]): CounselTechniqueInfo[] {
+    return counselTechniques.map((technique) => CounselTechniqueInfo.fromDomain(technique));
+  }
+}

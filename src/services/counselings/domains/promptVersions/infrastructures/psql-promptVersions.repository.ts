9302c5cf56c2cb69d00b1fccaa5f04bd2dd72abd
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-07-22 22:04:23 +0900

    mod: 관계 정리 메서드 삭제

--- src/services/counselings/domains/promptVersions/infrastructures/psql-promptVersions.repository.ts
+++ src/services/counselings/domains/promptVersions/infrastructures/psql-promptVersions.repository.ts
@@ -49,15 +49,11 @@ export class PsqlPromptVersionsRepository extends PromptVersionsRepository {
     if (Array.isArray(promptVersion)) {
       await this.promptVersionsRepository.save(PsqlPromptVersionsMapper.toEntities(promptVersion));
 
-      promptVersion.forEach((pv) => {
-        pv.removeDeletedRelations();
-      });
       return promptVersion;
     } else {
       const promptVersionEntity = PsqlPromptVersionsMapper.toEntity(promptVersion);
       await this.promptVersionsRepository.save(promptVersionEntity);
 
-      promptVersion.removeDeletedRelations();
       return promptVersion;
     }
   }

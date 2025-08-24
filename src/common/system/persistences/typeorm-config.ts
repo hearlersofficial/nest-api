a646b26e0c4e417c03ff6484fa1423bcc8db6f9f
astor-dev <orangnlp@gmail.com>
2025-08-24 18:38:39 +0900

    feat: db 마이그레이션 툴 제작

--- src/common/system/persistences/typeorm-config.ts
+++ src/common/system/persistences/typeorm-config.ts
@@ -1,25 +1,41 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import dotenv from "dotenv";
+import { resolve } from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+dotenv.config({ path: resolve(__dirname, "../../../../.env") });
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const dataSourceOptions: DataSourceOptions = {
+  type: "postgres",
+  host: process.env.POSTGRESQL_HOST,
+  port: parseInt(process.env.POSTGRESQL_PORT ?? "5432"),
+  username: process.env.POSTGRESQL_USER,
+  password: process.env.POSTGRESQL_PASSWORD,
+  database: process.env.POSTGRESQL_DATABASE,
+  entities: [__dirname + "/../**/*.entity.{js,ts}"],
+  synchronize: false,
+  migrations: [resolve(__dirname, "../../../migrations/**/*.ts")],
+  migrationsTableName: "typeorm_migrations",
+  logging: process.env.NODE_ENV === "development",
+  extra: {
+    options: "-c timezone=UTC",
+  },
+};
 
 @Injectable()
 export class TypeOrmConfigs implements TypeOrmOptionsFactory {
   constructor() {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    return {
-      type: "postgres",
-      host: process.env.POSTGRESQL_HOST,
-      port: parseInt(process.env.POSTGRESQL_PORT ?? "5432"),
-      username: process.env.POSTGRESQL_USER,
-      password: process.env.POSTGRESQL_PASSWORD,
-      database: process.env.POSTGRESQL_DATABASE,
-      entities: [__dirname + "/../**/*.entity.{js,ts}"],
-      synchronize: process.env.NODE_ENV === "development",
-      retryAttempts: 3,
-      logging: process.env.NODE_ENV === "development",
-      extra: {
-        options: "-c timezone=UTC",
-      },
-    };
+    return dataSourceOptions;
   }
 }
+
+export default new DataSource(dataSourceOptions);

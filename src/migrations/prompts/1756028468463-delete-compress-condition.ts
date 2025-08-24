9ddc749b7e3f8da7ad8b406c308ddb47b66bfce4
astor-dev <orangnlp@gmail.com>
2025-08-24 18:41:33 +0900

    fix: compress 조건 제거 마이그레이션

---
+++ src/migrations/prompts/1756028468463-delete-compress-condition.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DeleteCompressCondition1756028468463 implements MigrationInterface {
+    name = 'DeleteCompressCondition1756028468463'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "counsel_technique_transition_rules" DROP COLUMN "min_not_compressed_message_count"`);
+        await queryRunner.query(`ALTER TABLE "counsel_technique_transition_rules" DROP COLUMN "max_not_compressed_message_count"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "counsel_technique_transition_rules" ADD "max_not_compressed_message_count" integer`);
+        await queryRunner.query(`ALTER TABLE "counsel_technique_transition_rules" ADD "min_not_compressed_message_count" integer`);
+    }
+
+}

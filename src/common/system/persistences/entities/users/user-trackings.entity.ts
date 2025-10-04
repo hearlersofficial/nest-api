01ed9d0c533294bbe2aba3b92a051e47f9cabaa2
astor-dev <orangnlp@gmail.com>
2025-10-04 22:07:49 +0900

    feat: userTracking entity 추가

--- src/common/system/persistences/entities/users/user-trackings.entity.ts
+++ src/common/system/persistences/entities/users/user-trackings.entity.ts
@@ -1,17 +1,18 @@
 import { CoreEntity } from "~common/system/persistences/entities/core.entity";
 import { UsersEntity } from "~common/system/persistences/entities/users/users.entity";
-import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, RelationId } from "typeorm";
 
 @Entity({ name: "user_trackings" })
 export class UserTrackingsEntity extends CoreEntity {
   // 인트로 컷씬을 시청했는 지 유무
-  @Column({ type: "boolean", name: "intro_cutscene_watched" })
-  introCutsceneWatched: boolean;
+  @Column({ type: "boolean", name: "has_seen_intro_cutscene" })
+  hasSeenIntroCutscene: boolean;
 
+  @RelationId((userTrackings: UserTrackingsEntity) => userTrackings.user)
   @Column({ type: "bigint", name: "user_id" })
   userId: string;
 
-  @ManyToOne(() => UsersEntity)
+  @OneToOne(() => UsersEntity)
   @JoinColumn({ name: "user_id" })
   user: UsersEntity;
 }
